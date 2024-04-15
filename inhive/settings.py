c14cc0131233d02b195b633f6859951e310907e6
Pcosby5 <pcosby50@gmail.com>
2024-04-15 10:46:36 +0200

    ready for deployment

--- inhive/settings.py
+++ inhive/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = "django-insecure-73)#*6cz=xm2%a&*hr$z9g(%ryjptc^v)td75w43yp**bmjv6v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
