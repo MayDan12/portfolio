893de7612f2364b33c0d74b06c46ef04b6653a11
Pcosby5 <pcosby50@gmail.com>
2024-04-15 10:13:01 +0200

    ready for deployment

--- inhive/settings.py
+++ inhive/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
-
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-73)#*6cz=xm2%a&*hr$z9g(%ryjptc^v)td75w43yp**bmjv6v"
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
